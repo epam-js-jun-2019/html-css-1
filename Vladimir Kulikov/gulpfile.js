52773188f1fe154d76923c6a163d14939b00b805
kulikov98 <kulikov98@gmail.com>
2019-07-10 00:06:43 +0300

    add BEM

--- Vladimir Kulikov/gulpfile.js
+++ Vladimir Kulikov/gulpfile.js
@@ -1,16 +1,19 @@
 'use strict';
  
-var gulp = require('./node_modules/gulp');
-var sass = require('./node_modules/gulp-sass');
-
-sass.compiler = require('./node_modules/node-sass/lib');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
+   .pipe(sourcemaps.init())
+   .pipe(sass().on('error', sass.logError))
+   .pipe(sourcemaps.write('./maps'))
+   .pipe(gulp.dest('./css'));
+ });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/*.sass', gulp.parallel('sass'));
 });
+
